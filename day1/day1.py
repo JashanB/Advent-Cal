40a84cdcdf345364716070d7f5c924c0961f37cb
JashanB <jashan_brar50@hotmail.com>
2020-12-01 09:28:03 -0500

    part1 solved

--- day1/day1.py
+++ day1/day1.py
@@ -2,5 +2,8 @@ num_list = []
 num_file = open("numList.txt", "r")
 for line in num_file:
   num_list.append(int(line.rstrip("\n")))
-for num, index in enumerate(num_list):
-  
+for num1 in num_list:
+  for num2 in num_list:
+    new_num = num1 + num2
+    if new_num == 2020:
+      print(num1 * num2)
