3859c4c2be3b3f72752aed039dab480f183ee6c6
JashanB <jashan_brar50@hotmail.com>
2020-12-01 09:30:30 -0500

    finished part 2

--- day1/day1.py
+++ day1/day1.py
@@ -4,6 +4,7 @@ for line in num_file:
   num_list.append(int(line.rstrip("\n")))
 for num1 in num_list:
   for num2 in num_list:
-    new_num = num1 + num2
-    if new_num == 2020:
-      print(num1 * num2)
+    for num3 in num_list:
+      new_num = num1 + num2 + num3
+      if new_num == 2020:
+        print(num1 * num2 * num3)
